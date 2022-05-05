bf8161bbdf6a388651a9e7ed9427d3974ff8ded9
raviavanigadda <ravi.avanigadda95@gmail.com>
2022-05-05 18:15:15 +0530

    -Added coroutines

--- app/src/main/java/com/example/mealz/ui/meals/MainActivity.kt
+++ app/src/main/java/com/example/mealz/ui/meals/MainActivity.kt
@@ -6,14 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mealz.model.response.MealsResponse
 import com.example.mealz.ui.theme.MealzTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,12 +37,15 @@ fun MealsCategoriesScreen() {
     val rememberedMeals : MutableState<List<MealsResponse>> = remember {
         mutableStateOf(emptyList<MealsResponse>())
     }
-    viewModel.getMeals {
-        response ->
-        val mealsFromApi = response?.categories
-        rememberedMeals.value = mealsFromApi.orEmpty()
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = "GET_MEALS"){
+        coroutineScope.launch(Dispatchers.IO) {
+            val meals = viewModel.getMeals()
+            rememberedMeals.value = meals
+        }
     }
-    
+
     LazyColumn {
         items(rememberedMeals.value) {
             meal ->  Text(text = "${meal.name}")
